a8c38b4d0f6f6c93402a76d672359fab4ce01008
mallik-user1 <42467608+mallik-user1@users.noreply.github.com>
2021-11-25 12:44:25 +0530

    Update main.tf

--- terraform/aws/modules/storage/main.tf
+++ terraform/aws/modules/storage/main.tf
@@ -125,6 +125,7 @@ resource "aws_s3_bucket_public_access_block" "km_public_blob" {
   #zs:skip=AC_AWS_0215 ignore
   #zs:skip=AC_AWS_0214 ignore
   #zs:skip=AC_AWS_0207 ignore
+  # nothing else
   bucket = aws_s3_bucket.km_public_blob.id
 
   block_public_acls   = false
