f2a87b0ae4128ef3cf6549a1c7f3c0368f3f76e0
mallik-user1 <42467608+mallik-user1@users.noreply.github.com>
2021-11-30 18:17:38 +0530

    Create newFileCheck.tf

---
+++ newFileCheck.tf
@@ -0,0 +1,21 @@
+resource "aws_s3_bucket" "km_blob_storage" {
+  #zs:skip=AC_AWS_0207 ignore
+  #zs:skip=AC_AWS_0497 ignore 
+  #zs:skip=AC_AWS_0214 ignore
+  bucket = "km-blob-storage-${var.environment}"
+  acl    = "private"
+  tags = merge(var.default_tags, {
+    name = "km_blob_storage_${var.environment}"
+  })
+}
+
+resource "aws_s3_bucket" "km_public_blob" {
+  bucket = "km-public-blob"
+}
+
+resource "aws_s3_bucket_public_access_block" "km_public_blob" {
+  bucket = aws_s3_bucket.km_public_blob.id
+
+  block_public_acls   = false
+  block_public_policy = false
+}
