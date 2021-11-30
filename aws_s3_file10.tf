c7fbb8455c9b14037cdab1467f98628e1440fe19
mallik-user1 <42467608+mallik-user1@users.noreply.github.com>
2021-11-30 19:01:44 +0530

    Add files via upload

---
+++ aws_s3_file10.tf
@@ -0,0 +1,21 @@
+resource "aws_s3_bucket" "km_blob_storage" {
+  bucket = "km-blob-storage-${var.environment}"
+  acl    = "private"
+  tags = merge(var.default_tags, {
+    name = "km_blob_storage_${var.environment}"
+  })
+}
+
+resource "aws_s3_bucket" "km_public_blob" {
+  #zs:skip=AC_AWS_0207 ignore
+  #zs:skip=AC_AWS_0214 ignore
+  #zs:skip=AC_AWS_0497 ignore
+  bucket = "km-public-blob"
+}
+
+resource "aws_s3_bucket_public_access_block" "km_public_blob" {
+  bucket = aws_s3_bucket.km_public_blob.id
+
+  block_public_acls   = false
+  block_public_policy = false
+}
